d93c685823edcfa31a67ca0bccd16f2556d0e051
Jiye-Choe <hyde69ciel@gmail.com>
2020-11-19 00:14:01 +0900

    #10. 사용자인증 커스터마이징

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,19 +1,36 @@
 package tacos.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(encoder());
+	}
+	
+	
+	/*
 	@Autowired
 	DataSource dataSource;
 	
@@ -28,6 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.httpBasic();
 	}
 	
+	
+	//LDAP 기반 사용자스토어
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
@@ -47,7 +66,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	//JDBC 기반 사용자스토어
-	/*
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
