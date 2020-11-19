90f8979eb603ad84825f4711727fe757be22e7cd
Jiye-Choe <hyde69ciel@gmail.com>
2020-11-19 15:35:32 +0900

    #12. 웹 요청 보안 처리(로그인)

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,6 +28,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth
 			.userDetailsService(userDetailsService)
 			.passwordEncoder(encoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/design", "/orders")	//제한요소가 먼저
+			.hasRole("ROLE_USER")
+			.antMatchers("/","/**").permitAll()	//모두 허용은 나중에
+			.and()	//인증 구성이 끝났다. 새로운 구성을 시작한다는 뜻
+			.formLogin()
+			.loginPage("/login");	//커스텀 로그인 페이지 경로
 	}
 	
 	
