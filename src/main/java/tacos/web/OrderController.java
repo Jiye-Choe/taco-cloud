e108b492b49a6c6974368745f9140981ced5d412
Jiye-Choe <hyde69ciel@gmail.com>
2020-12-12 17:50:54 +0900

    #15. 구성속성

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,6 +2,9 @@ package tacos.web;
 
 import javax.validation.Valid;
 import org.springframework.validation.Errors;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,12 +24,16 @@ import tacos.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
+	
+	private OrderProps props;
 	
 	private OrderRepository orderRepo;
 	
-	public OrderController(OrderRepository orderRepo) {
+	public OrderController(OrderRepository orderRepo, OrderProps props) {
 		this.orderRepo = orderRepo;
+		this.props = props;
 	}
 
 	@GetMapping("/current")
@@ -52,16 +59,22 @@ public class OrderController {
 	  }
 
 	@PostMapping
-	  public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
+	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
 			  @AuthenticationPrincipal User user) {
-		if (errors.hasErrors()) {
-		    return "orderForm";
-		}
+			if (errors.hasErrors()) {
+				return "orderForm";
+			}
 		order.setUser(user);
 		
 		orderRepo.save(order);
 		sessionStatus.setComplete();
-	    return "redirect:/";
-	  }
+		return "redirect:/";
+	 }
 
+	@GetMapping
+	public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+		Pageable pageable = PageRequest.of(0, props.getPageSize());
+		model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+		return "orderList";
+	}
 }
